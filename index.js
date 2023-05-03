1c6abff3dac8599724e8e6a70ba6ece3f482fb04
Gini Huang <126279974+sweetiegini@users.noreply.github.com>
2023-05-03 22:09:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import "bootswatch/dist/[Lumen]/bootstrap.min.css";
+import React from 'react';
+import logo from './logo.svg';
+import 'bootswatch/dist/slate/bootstrap.min.css'; // Added this :boom:
+import './App.css';
